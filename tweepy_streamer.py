8c06ef8e079d2f18ce99e65290df3ae5e2514204
brandon-fenty <brandonfenty@gmail.com>
2020-03-03 16:42:04 -0800

    putting tweets into data frames

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -5,13 +5,31 @@ from tweepy import API
 from tweepy import Cursor
 
 import twitter_credentials
+import numpy as np
+import pandas as pd
 # This application was coded by following  the "Tweet Visualization and Sentiment Analysis in Python Tutorial"
 # from Vincent Russo of Lucid Programming
 
 
-class TwitterAuth:
+class TwitterClient:
+    def __init__(self, twitter_user=None):
+        self.auth = TwitterAuth().authenticate_twitter()
+        self.twitter_client = API(self.auth)
+        self.twitter_user = twitter_user
 
-    def authenticate_twitter(self):
+    def get_twitter_client_api(self):
+        return self.twitter_client
+
+    def get_user_timeline_tweets(self, num_tweets):
+        tweets_list = []
+        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
+            tweets_list.append(tweet)
+        return tweets_list
+
+
+class TwitterAuth:
+    @staticmethod
+    def authenticate_twitter():
         auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
         auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
         return auth
@@ -51,14 +69,36 @@ class TwitterListener(StreamListener):
         return True
 
     def on_error(self, status):
+        if status == 420:
+            # Terminate connection if twitter rate limit is exceeded
+            return False
         print(status)
-        return True
+
+
+class TweetAnalyzer:
+    """
+    Used to analyze tweet data and categorize it
+    """
+    def tweets_to_df(self, tweets):
+        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweets'])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['favorites'] = np.array([tweet.favorite_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+
+        return df
 
 
 if __name__ == "__main__":
 
-    hash_tags = ["libertymutual", "limu", "safeco", "insurance"]
-    fetched_tweets_file = "tweets.json"
+    twitter_client = TwitterClient()
+    tweet_analyzer = TweetAnalyzer()
 
-    twitter_streamer = TwitterStreamer()
-    twitter_streamer.stream_tweets(fetched_tweets_file, hash_tags)
+    api = twitter_client.get_twitter_client_api()
+    tweets = api.user_timeline(screen_name="realDonaldTrump", count=200)
+
+    df = tweet_analyzer.tweets_to_df(tweets)
+    # print("Average likes - ", np.mean(df['favorites']))
+    # print("Most likes - ", np.max(df['favorites']))
+    # print(dir(tweets[0]))
+    print(df.head(20))
+    # print(tweets[0].in_reply_to_screen_name)
